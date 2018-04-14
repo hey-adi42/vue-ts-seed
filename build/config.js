ebd58c7b914a7026285107f5d4cf4675d02884b8
liudi <liudi10@meituan.com>
2018-04-14 21:36:41 +0800

    Feat：修改css声明lang打包报错，及升级依赖包版本

--- build/config.js
+++ build/config.js
@@ -18,5 +18,5 @@ module.exports = {
     },
     // when you use electron please set to relative path like ./
     // otherwise only set to absolute path when you're using history mode
-    publicPath: '/',
+    publicPath: './',
 };
